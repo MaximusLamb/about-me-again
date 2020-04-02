cfdf57db1b6cbe79e3d218a2bd817e372b1f2f3d
keegers <nikkikiga@gmail.com>
2020-04-02 15:43:43 -0700

    update tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,35 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import isYes from '../isYes.js';
 
 const test = QUnit.test;
+const expected = false;
+const answer = 'chicken';
 
-test('time to test a function', function(assert) {
+test('Test if isYes for yes', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const expected = true;
+    const answer = 'yes';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = isYes(answer);
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, expected);
+});
+
+test('Test if isYes for chicken', function(assert) {
     //Arrange
     // Set up your parameters and expectations
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const result = isYes(answer);
+
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(result, expected);
 });
